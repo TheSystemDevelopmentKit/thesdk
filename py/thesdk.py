141cd282c1dbb4e6d243da841a563d23cef9e944
Marko Kosunen <marko.kosunen@aalto.fi>
2017-09-26 17:43:53 -0700

    Add Entities/thesdk/py/thesdk.py to enable parameter propagation
    MAJOR: Parameter propagation verified with Simulations/Inverter_py

---
+++ py/thesdk.py
@@ -0,0 +1,32 @@
+# THESDK class 
+# Provides commmon methods  for other classes TheSDK
+#
+##############################################################################
+import numpy as np
+from refptr import *
+
+class thesdk:
+    def copy_propval(self,*arg):
+        if len(arg)>=2:
+            self.parent=arg[0]
+            self.proplist=arg[1]
+            for i in range(1,len(self.proplist)+1):
+                if hasattr(self,self.proplist[i-1]):
+                    setattr(self,self.proplist[i-1],getattr(self.parent,self.proplist[i-1]))
+
+# Last modification by Marko Kosunen, marko.kosunen@aalto.fi, 26.09.2017 17:33
+#classdef (Abstract) thesdk < handle
+#    methods    
+#        %To copy the properties from the parent
+#        function obj=copy_propval(obj,varargin)
+#            parent=varargin{1};
+#            proplist=varargin{2};
+#            for i = 1:length(proplist);
+#                if isprop(parent,char(proplist{i}))==1 && isprop(obj,char(proplist{i}))==1
+#                    obj.(char(proplist{i})) = parent.(char(proplist{i}));
+#                end
+#            end
+#        end
+#    end
+#end
+
