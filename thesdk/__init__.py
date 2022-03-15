18162b8722a08d79b37f104bc3252c8a95264a74
Santeri Porrasmaa <santeri.porrasmaa@aalto.fi>
2022-03-15 08:21:32 +0200

    Do not save _par when saving state
    This enables loading results of parallel runs individually. Also, filter
    properties _par, _queue and generator when loading state. This enables
    loading the results generated by older versions.

--- thesdk/__init__.py
+++ thesdk/__init__.py
@@ -783,12 +783,16 @@ class thesdk(metaclass=abc.ABCMeta):
 
     def __getstate__(self):
         state=self.__dict__.copy()
-        exclude_list = ['_queue', 'generator']
+        exclude_list = ['_par', '_queue', 'generator']
         for item in exclude_list: 
             if item in state:
                 del state[item]
         return state
     def __setstate__(self,state):
+        exclude_list = ['_par', '_queue', 'generator']
+        for item in exclude_list:
+            if item in state:
+                del state[item]
         self.__dict__.update(state)
 
     @property
