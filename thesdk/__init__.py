85950ecbd306e2745a260f48509cb180817595e3
Santeri Porrasmaa <santeri.porrasmaa@aalto.fi>
2023-08-16 08:54:48 +0300

    Fix accesing attribute 'simpath' creating empty directories
    Previously, simpath would check if runname was set non-empty
    string to determine whether or not we are loading results or
    running new simulations. This seems to create a new random path,
    since the attribute is accessed. The correct way is to check
    if the attribute exists or not.

--- thesdk/__init__.py
+++ thesdk/__init__.py
@@ -224,7 +224,12 @@ class thesdk(metaclass=abc.ABCMeta):
         This is not meant to be set manually.
         """
         #This property is dependent, it should not be fixed in creation
-        name = self.runname if self.runname != '' else self.load_state
+        if hasattr(self, '_runname'):
+            name = self.runname
+        elif hasattr(self, '_load_state'):
+            name = self.load_state
+        else:
+            self.print_log(type='F', msg='Entity runname or load_state not set! This shouldn\'t happen')
         self._simpath = '%s/simulations/%s/%s' % (self.entitypath,self.model,name)
         try:
             if not (os.path.exists(self._simpath)):
