b6c7d2a788493ea905871926f6010950d6b4df8c
Aleksi Korsman <aleksi.korsman@aalto.fi>
2025-11-03 16:44:33 +0200

    Use special function when connectiong two IOs

--- thesdk/bundle.py
+++ thesdk/bundle.py
@@ -2,6 +2,17 @@
 # Class is needed to define bundle operations
 import abc
 from abc import *
+import thesdk
+
+
+class IODict(dict):
+    """Dictionary that automatically connects IOs instead of replacing them."""
+    def __setitem__(self, key, value):
+        # If both sides are IOs, perform connection instead of reassignment
+        if key in self and isinstance(self[key], thesdk.IO) and isinstance(value, thesdk.IO):
+            self[key].connect(value)
+        else:
+            super().__setitem__(key, value)
 
 
 class Bundle(metaclass=abc.ABCMeta):
@@ -26,7 +37,7 @@ class Bundle(metaclass=abc.ABCMeta):
         Members: dict, dict([])
 
         """
-        self.Members = dict([])
+        self.Members = IODict([])
 
     def new(self, **kwargs):
         """Parameters
