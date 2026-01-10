c880e0a6d297e35ecdc23e94c66086794fe35277
Marko Kosunen <marko.kosunen@aalto.fi>
2026-01-10 09:52:40 +0200

    Disable precommit hooks for a while

--- thesdk/__init__.py
+++ thesdk/__init__.py
@@ -20,6 +20,7 @@ and in every way we are getting better and better :).
 
 """
 
+
 import sys
 import os
 import glob
@@ -161,16 +162,16 @@ class thesdk(metaclass=abc.ABCMeta):
         if os.path.isfile(__class__.logfile):
             os.remove(__class__.logfile)
         typestr = "[INFO]"
-        # Colors for stdout prints
+        # Colors for stdout prints. Disable linter check for unused variable
         cend = "" if not cls.print_colors else "\33[0m"
-        cblack = "" if not cls.print_colors else "\33[30m"
-        cred = "" if not cls.print_colors else "\33[31m"
-        cgreen = "" if not cls.print_colors else "\33[32m"
-        cyellow = "" if not cls.print_colors else "\33[33m"
-        cblue = "" if not cls.print_colors else "\33[34m"
-        cviolet = "" if not cls.print_colors else "\33[35m"
-        cbeige = "" if not cls.print_colors else "\33[36m"
-        cwhite = "" if not cls.print_colors else "\33[37m"
+        cblack = "" if not cls.print_colors else "\33[30m" # noqa: F841
+        cred = "" if not cls.print_colors else "\33[31m" # noqa: F841
+        cgreen = "" if not cls.print_colors else "\33[32m" # noqa: F841
+        cyellow = "" if not cls.print_colors else "\33[33m" # noqa: F841
+        cblue = "" if not cls.print_colors else "\33[34m" # noqa: F841
+        cviolet = "" if not cls.print_colors else "\33[35m" # noqa: F841
+        cbeige = "" if not cls.print_colors else "\33[36m" # noqa: F841
+        cwhite = "" if not cls.print_colors else "\33[37m" # noqa: F841
         msg = "Default logfile override. Initialized logging in %s" % (
             __class__.logfile
         )
@@ -499,16 +500,16 @@ class thesdk(metaclass=abc.ABCMeta):
             if hasattr(self, "parent"):
                 msg = msg.replace(self.parent.entitypath, ".")
 
-        # Colors for stdout prints
+        # Colors for stdout prints. Disable linter check for unused variable..
         cend = "" if not self.print_colors else "\33[0m"
-        cblack = "" if not self.print_colors else "\33[30m"
+        cblack = "" if not self.print_colors else "\33[30m" # noqa: F841
         cred = "" if not self.print_colors else "\33[31m"
         cgreen = "" if not self.print_colors else "\33[32m"
         cyellow = "" if not self.print_colors else "\33[33m"
         cblue = "" if not self.print_colors else "\33[34m"
         cviolet = "" if not self.print_colors else "\33[35m"
-        cbeige = "" if not self.print_colors else "\33[36m"
-        cwhite = "" if not self.print_colors else "\33[37m"
+        cbeige = "" if not self.print_colors else "\33[36m" # noqa: F841
+        cwhite = "" if not self.print_colors else "\33[37m" # noqa: F841
 
         if not os.path.isfile(thesdk.logfile):
             typestr = "[INFO]"
@@ -917,6 +918,8 @@ class thesdk(metaclass=abc.ABCMeta):
 
         List of strings containing the parameters of a netlist. List is populated by calling
         ecd_methods.get_params(). Empty if no parameters were found in the netlist.
+
+        [TODO] This creates a link to something (ecd_methods) that is not in open domain. Refine.
         """
         if not hasattr(self, "_netlist_params"):
             self._netlist_params = []
@@ -924,12 +927,12 @@ class thesdk(metaclass=abc.ABCMeta):
 
     @netlist_params.setter
     def netlist_params(self, value):
-        if isinstance(val, list):
+        if isinstance(value, list):
             self._netlist_params = value
         else:
             self.print_log(
                 type="W",
-                msg="Cannot set property netlist_params as type %s" % type(val),
+                msg="Cannot set property netlist_params as type %s" % type(value),
             )
 
     @property
@@ -1074,7 +1077,6 @@ class thesdk(metaclass=abc.ABCMeta):
 
         This should be called after the simulation has finished.
         """
-        pathname = "%s/%s" % (self.statepath, self.runname)
         try:
             if not (os.path.exists(self.statedir)):
                 os.makedirs(self.statedir)
